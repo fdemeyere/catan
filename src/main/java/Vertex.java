c1db8f820654db316f1d6aa5072e46d3341029a2
fdemeyere <152783398+fdemeyere@users.noreply.github.com>
2024-12-19 23:53:24 +0100

    Update Vertex.java

--- src/main/java/Vertex.java
+++ src/main/java/Vertex.java
@@ -3,14 +3,14 @@ import java.util.ArrayList;
 
 public class Vertex {
 
-    int id;
+    VertexID id = new VertexID();
 
     List<Cube> cubes = new ArrayList<>();
 
     boolean buildable = true;
 
-    Vertex(int id){
-        this.id = id;
+    Vertex(){
+
     }
 
 
