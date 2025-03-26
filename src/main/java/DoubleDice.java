7a0c9d2f3558a584e722f0cc78cc5b62b90e8975
Filip de Meyere <filipjdemeyere@gmail.com>
2025-03-26 01:16:46 +0100

    Add dice

---
+++ src/main/java/DoubleDice.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class DoubleDice {
+    public static class DiceRollResult {
+        public static int die1;
+        public static int die2;
+        public static int sum;
+
+        public DiceRollResult(int die1, int die2, int sum) {
+            DoubleDice.DiceRollResult.die1 = die1;
+            DoubleDice.DiceRollResult.die2 = die2;
+            DoubleDice.DiceRollResult.sum = sum;
+        }
+
+    }
+
+    public static DiceRollResult DiceRoll() {
+        Random random = new Random();
+
+        int die1 = random.nextInt(6) + 1;
+        int die2 = random.nextInt(6) + 1;
+        int sum = die1 + die2;
+        return new DiceRollResult(die1, die2, sum);
+    }
+
+    public static void drawResult(Graphics2D g2d) {
+        g2d.setColor(new Color(26, 26, 26));
+        g2d.drawString("" + DiceRollResult.die1 + ", " + DiceRollResult.die2, 10, 40);
+
+    }
+
+}
